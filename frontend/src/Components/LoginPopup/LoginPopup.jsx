1343bd4b87a69645214910e4fc581d8bfefb4f55
otienojoseph <otienoj.cruz@gmail.com>
2024-09-24 02:42:13 +0300

    fix: styling issues

--- frontend/src/Components/LoginPopup/LoginPopup.jsx
+++ frontend/src/Components/LoginPopup/LoginPopup.jsx
@@ -1,34 +1,49 @@
-import React, {useState} from 'react'
-import { assets } from '../../assets/assets'
-import './LoginPopup.css'
-import { Image } from '@chakra-ui/react'
+import { useState } from "react";
+import { assets } from "../../assets/assets";
+import "./LoginPopup.css";
 
-function LoginPopup({setShowLogin}) {
-
-    const [currentState, setCurrentState] = useState('Login')
-  return (
-    <div className='login-popup'>
-        <from className="login-popup-container">
-            <div className="login-popup-title">
-                <h2>{currentState}</h2>
-                <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
-            </div>
-            <div className="login-popup-input">
-                {currentState==='Login'?<></>:<input type="text" placeholder='Fullname' required />}
-                <input type="email" placeholder='Your email' required />
-                <input type="password" placeholder='Your password' required />
-                <input type="password" placeholder='Confirm password' required />
-            </div>
-            <button>{currentState==='Sign Up'?"Sign up":'Login'}</button>
-            <div className="login-popup-condition">
-                <input type="checkbox" required />
-                <p>By continuing, I agree to the terms of use & privacy policy.</p>
-            </div>
-            {currentState==='Login'?<p>Create a new account? <span onClick={()=>setCurrentState('Sign Up')}>Click here</span></p>:
-            <p>Already have an account? <span onClick={()=>setCurrentState('Login')}>Login here</span></p>}
-        </from>
-    </div>
-  )
+function LoginPopup({ setShowLogin }) {
+	const [currentState, setCurrentState] = useState("Login");
+	return (
+		<div className="login-popup" onClick={() => setShowLogin((prev) => !prev)}>
+			<form className="login-popup-container">
+				<div className="login-popup-title">
+					<h2>{currentState}</h2>
+					<img
+						onClick={() => setShowLogin((prev) => !prev)}
+						src={assets.cross_icon}
+						alt=""
+					/>
+				</div>
+				<div className="login-popup-input">
+					{currentState === "Login" ? (
+						<></>
+					) : (
+						<input type="text" placeholder="Fullname" required />
+					)}
+					<input type="email" placeholder="Your email" required />
+					<input type="password" placeholder="Your password" required />
+					<input type="password" placeholder="Confirm password" required />
+				</div>
+				<button>{currentState === "Sign Up" ? "Sign up" : "Login"}</button>
+				<div className="login-popup-condition">
+					<input type="checkbox" required />
+					<p>By continuing, I agree to the terms of use & privacy policy.</p>
+				</div>
+				{currentState === "Login" ? (
+					<p>
+						Create a new account?{" "}
+						<span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+					</p>
+				) : (
+					<p>
+						Already have an account?{" "}
+						<span onClick={() => setCurrentState("Login")}>Login here</span>
+					</p>
+				)}
+			</form>
+		</div>
+	);
 }
 
-export default LoginPopup
+export default LoginPopup;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import ErrorPage from "./routes/Error.jsx";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		errorElement: <ErrorPage />,
+		children: [{}],
+	},
+]);
 
 createRoot(document.getElementById("root")).render(
-	<ChakraProvider>
-		<StrictMode>
-			<App />
-		</StrictMode>
-	</ChakraProvider>
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
 );
